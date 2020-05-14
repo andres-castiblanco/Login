7b0bae58df1429cf41d39344f3a7c5775f97b27e
andres-castiblanco <37271641+andres-castiblanco@users.noreply.github.com>
2020-05-14 16:02:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ function clicked() {
 	var user = document.getElementById('username');
 	var pass = document.getElementById('password');
 	
-	var coruser = "test";
-	var corpass = "123";
+	var coruser = "Quindio";
+	var corpass = "200514";
 	
 	if(user.value == coruser) {
 	
 		if(pass.value == corpass) {
 		
 			window.alert("You are logged in as " + user.value);
-			window.open("https://andres-castiblanco.github.io/COVID-19-COL/","_self");
+			window.open("https://andres-castiblanco.github.io/Quindio/","_self");
 		
 		} else {
 		
